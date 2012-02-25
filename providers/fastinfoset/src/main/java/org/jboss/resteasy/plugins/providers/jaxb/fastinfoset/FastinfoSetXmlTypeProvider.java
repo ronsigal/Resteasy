fa309112b7eb504aa05ba2523948b9009227f15e
Ron Sigal <rsigal@redhat.com>
2012-02-24 19:37:40 -0500

    RESTEASY-659: Added providers and unit tests.

---
+++ providers/fastinfoset/src/main/java/org/jboss/resteasy/plugins/providers/jaxb/fastinfoset/FastinfoSetXmlTypeProvider.java
@@ -0,0 +1,30 @@
+package org.jboss.resteasy.plugins.providers.jaxb.fastinfoset;
+
+import org.jboss.resteasy.plugins.providers.jaxb.JAXBXmlTypeProvider;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+@Provider
+@Consumes("application/*+fastinfoset")
+@Produces("application/*+fastinfoset")
+public class FastinfoSetXmlTypeProvider extends JAXBXmlTypeProvider
+{
+   /**
+    * In which, by "true, we mean "false".
+    * If the context parameter "resteasy.document.expand.entity.references"
+    * is set to "false", JAXBElementProvider will wrap the Unmarshaller
+    * with an ExternalEntityUnmarshaller, which interferes with 
+    * FastinfoSet unmarshalling.  The FastinfoSet implementation currently
+    * in use doesn't expande external entity references, in any case.
+    */
+   public boolean isExpandEntityReferences()
+   {
+      return true;
+   }
+}
