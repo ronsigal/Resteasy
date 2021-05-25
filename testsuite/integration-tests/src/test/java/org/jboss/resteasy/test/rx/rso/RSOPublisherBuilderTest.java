17f8525d73ba20d2e3a1de506629abb8c3d77087
Ron Sigal <rsigal@redhat.com>
2021-05-25 14:12:34 -0400

    [EAP7-1661] Checkstyle fix

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/rx/rso/RSOPublisherBuilderTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/rx/rso/RSOPublisherBuilderTest.java
@@ -36,7 +36,6 @@ import org.jboss.resteasy.test.rx.rso.resource.RSOPublisherResourceImpl;
 import org.jboss.resteasy.utils.PortProviderUtil;
 import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.AfterClass;
 import org.junit.Assert;
