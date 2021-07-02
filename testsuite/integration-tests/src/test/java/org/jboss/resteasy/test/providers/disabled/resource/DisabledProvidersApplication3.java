a4338fc39dd3bf7f61b63ad6111a3bf558ef4397
Ron Sigal <rsigal@redhat.com>
2021-07-02 16:45:10 -0400

    Nuked tests

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/disabled/resource/DisabledProvidersApplication3.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.resteasy.test.providers.disabled.resource;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class DisabledProvidersApplication3 extends Application {
-
-}
--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/validation/ejb/resource/EJBParameterViolationsOnlyTestApplication.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.resteasy.test.validation.ejb.resource;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("app")
-public class EJBParameterViolationsOnlyTestApplication extends Application
-{
-}
