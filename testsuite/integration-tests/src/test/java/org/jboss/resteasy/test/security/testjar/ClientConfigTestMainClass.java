df128417da2023961ad9efb27240507e809574a1
Ron Sigal <rsigal@redhat.com>
2021-07-06 21:12:20 -0400

    Timeout

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/security/testjar/ClientConfigTestMainClass.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/security/testjar/ClientConfigTestMainClass.java
@@ -1,22 +1,50 @@
 package org.jboss.resteasy.test.security.testjar;
 
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.internal.BasicAuthentication;
-
-import javax.net.ssl.SSLContext;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.internal.BasicAuthentication;
 
 /**
  * ClientConfigProvider implementation used in jar that tests ClientConfigProvider functionality regarding HTTP BASIC auth and SSLContext.
  */
+
 public class ClientConfigTestMainClass {
     public static void main(String[] args) throws IOException, URISyntaxException, NoSuchAlgorithmException {
+       ExecutorService executor = Executors.newSingleThreadExecutor();
+       Future<String> future = executor.submit(new Callable<String>() {
+
+           public String call() throws Exception {
+              runMain(args);
+              return "OK";
+           }
+       });
+       try {
+        //CHECKSTYLE.OFF
+           System.out.println(future.get(10, TimeUnit.SECONDS)); //timeout is in 2 seconds
+         //CHECKSTYLE.ON
+       } catch (Exception e) {
+        //CHECKSTYLE.OFF
+           System.err.println("Timeout");
+         //CHECKSTYLE.ON
+       }
+       executor.shutdownNow();
+    }
+    
+    static void runMain(String[] args) throws Exception {
         if (args.length <= 1) {
             throw new IllegalArgumentException("Url must be supplied!");
         }
