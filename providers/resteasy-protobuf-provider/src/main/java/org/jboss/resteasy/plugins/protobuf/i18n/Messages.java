e2bc74dcee726010f4384306d388a053ab2194f9
Ron Sigal <rsigal@redhat.com>
2021-10-10 22:19:16 -0400

    Adding a note to resteasy-protobuf-provider/README.md

--- providers/resteasy-protobuf-provider/src/main/java/org/jboss/resteasy/plugins/protobuf/i18n/Messages.java
+++ providers/resteasy-protobuf-provider/src/main/java/org/jboss/resteasy/plugins/protobuf/i18n/Messages.java
@@ -4,11 +4,7 @@ import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageBundle;
 
 /**
- *
- * @author <a href="ron.sigal@jboss.com">Ron Sigal</a>
- * @version $Revision: 1.1 $
- *
- * Copyright Aug 25, 2015
+ * 
  */
 @MessageBundle(projectCode = "RESTEASY")
 public interface Messages
